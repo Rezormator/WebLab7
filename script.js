f4d3c26152a04a00d5c8ee70db7ce1b211f58dcb
Rezor <kostiatsymbal2019@gmail.com>
2024-12-06 18:26:44 +0200

    Init

--- script.js
+++ script.js
@@ -146,7 +146,7 @@ function logEvent(eventDescription) {
     events.push(event);
     localStorage.setItem('events', JSON.stringify(events));
 
-    fetch('https://weblabserver7.railway.internal/log_event', {
+    fetch('https://weblabserver7-production.up.railway.app/log_event', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(event),
@@ -165,7 +165,7 @@ function logBatchEvents() {
     const events = JSON.parse(localStorage.getItem('events')) || [];
     if (events.length === 0) return;
 
-    fetch('https://weblabserver7.railway.internal/log_batch', {
+    fetch('https://weblabserver7-production.up.railway.app/log_batch', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(events)
